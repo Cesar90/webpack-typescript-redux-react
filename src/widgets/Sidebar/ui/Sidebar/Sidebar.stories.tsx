448b899827c130f69dbea21939f33036f8076ee3
Cesar <cesarc@nicasource.com>
2023-07-06 12:40:05 -0600

    webpack plugin dev

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import Sidebar  from './Sidebar';
+import Sidebar from './Sidebar';
 
 export default {
     title: 'widget/Sidebar',
