15f6205a80365ea080d4fcbed1c47d0b14eb3154
Cesar <cesarc@nicasource.com>
2023-07-07 11:59:53 -0600

    loki files added

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { withTranslation } from 'react-i18next';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
 import Sidebar from './Sidebar';
 
@@ -7,12 +8,12 @@ describe('Sidebar', () => {
     test('With only', () => {
     // const SidebarWithTranslation = withTranslation()(Sidebar);
     // render(<SidebarWithTranslation />)
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('test toggle', () => {
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
