c55e8a09ef263388b2b20af08279ec3e59beef87
Cesar <cesarc@nicasource.com>
2023-07-16 21:16:15 -0600

    add lazy loading, add reducer manager, add dynamic module loader component

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,21 +1,26 @@
-import { DeepPartial } from '@reduxjs/toolkit';
 import React, { ReactNode } from 'react';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import { StateSchema } from '../config/StateSchema';
 import { createReduxStore } from '../config/store';
 
 interface StoreProviderProps{
-  children?: ReactNode,
-  initialState?: DeepPartial<StateSchema>
+  children?: ReactNode;
+  initialState?: DeepPartial<StateSchema>;
+  asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>
 }
 
 export const StoreProvider = (props: StoreProviderProps) => {
     const {
         children,
         initialState,
+        asyncReducers,
     } = props;
 
-    const store = createReduxStore(initialState as StateSchema);
+    const store = createReduxStore(
+        initialState as StateSchema,
+        asyncReducers as ReducersMapObject<StateSchema>,
+    );
 
     return (
         <Provider store={store}>
